dee6da211b93212e7964d5d5561e776c2c1db44a
themmm19 <melhannach@gmail.com>
2023-03-28 14:27:35 -0700

    project1

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+//Creates an alert that says hello
+window.alert("Hello!") ;
+// This is a line break
+document.write("<br>");
+
+//Writes the initial greeting
+document.write("Hope you\'re doing well!") ;
+// This is a line break
+document.write("<br>");
+
+//Variables to be concatenated
+var Sent1 = "This is the beginning of the string"; 
+
+var Sent2 = " and this is the end of the string" ;
+
+//Concatenation of Variables Sent1 and Sent2
+document.write(Sent1 + Sent2) ;
+// This is a line break
+document.write("<br>");
+
+//Writes the expressions and executes
+document.write(200 * 25);
+// This is a line break
+document.write("<br>");
